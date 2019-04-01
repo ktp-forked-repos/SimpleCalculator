bee3bb6f262af85610c47728adcf4b9fb909060e
Shivam Patel <shivampatel066@gmail.com>
2019-04-01 19:50:34 +0530

    Simple Calculator
    UI and Logic Completed !!

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -10,11 +10,81 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    var isFirstDigit = true
+    var operation1 : Double = 0
+    var operation2 = "="
+    var displayValue : Double {
+        get {
+            
+            return NumberFormatter().number(from: label.text!)!.doubleValue
+        }
+        set {
+            label.text = String(format: "%4.0f", newValue)
+            isFirstDigit = true
+            operation2 = "="
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func digitPressed(_ sender: AnyObject) {
+        
+        let digit = sender.currentTitle!
+        
+        label.text = isFirstDigit ? digit : label.text! + digit!
+        isFirstDigit = false
+        
+        
+        
+        
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        
+        displayValue = 0
+        
+    }
+    
 
-
+    @IBAction func operation(_ sender: AnyObject) {
+        
+        operation2 = sender.currentTitle!
+        operation1 = displayValue
+        isFirstDigit = true
+        
+        
+        
+        
+    }
+    @IBAction func calculation(_ sender: Any) {
+        
+        switch operation2 {
+        
+        case "+":
+            displayValue += operation1
+        
+        case "-":
+            displayValue = operation1 - displayValue
+            
+        case "*":
+            displayValue *= operation1
+            
+        case "/":
+            displayValue = operation1 / displayValue
+            
+        default:
+            break
+        }
+        
+    }
+    
 }
 
